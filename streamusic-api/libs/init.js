318199c44c9b3f84774f15f97dabcd718ff28319
omeroot <demircanomer91@gmail.com>
2015-09-18 10:18:10 +0300

    remove serve

--- streamusic-api/libs/init.js
+++ streamusic-api/libs/init.js
@@ -3,9 +3,6 @@ var express = require('express');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var tverify = require('./middlewares/token-verify.js');
-var login = require('./modules/login.js');
-var staticServe = require('./modules/static-serve.js');
-var register = require('./modules/register.js');
 var mongoose = require('mongoose');
 var conf = require('../configure/conf.js');
 var port = require('../configure/conf').port;
@@ -21,21 +18,10 @@ mongoose.connect(conf.database.host);
 app.use(cookieParser());
 app.use(bodyParser());
 
-///
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
-////
-
 apiRouter.use(tverify);
 
 app.use('/api/v1', apiRouter);
 app.use('/auth', authRouter);
-
-/////
-app.post('/login',login);
-app.post('/register',register);
-app.get('*',staticServe.index);
-/////
 
 server.listen(port);
 
